6be67fab5caf1ec0b7e5bbb773ae8614903b6913
Davi S. <davialvessampaio00@gmail.com>
2024-12-02 22:09:27 -0300

    refactor: make path variables less hard coded
    Add the path to the script itself and source the global control to update the variable paths

--- Configs/.local/lib/hyde/cyclewallpaperandtheme.sh
+++ Configs/.local/lib/hyde/cyclewallpaperandtheme.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
+scrDir=$(dirname "$(realpath "$0")")
+source "$scrDir/globalcontrol.sh"
+
 # Paths to scripts and directories
-WALL_SCRIPT="$HOME/.local/share/bin/swwwallpaper.sh"
-THEME_SCRIPT="$HOME/.local/share/bin/themeswitch.sh"
+WALL_SCRIPT="$scrDir/swwwallpaper.sh"
+THEME_SCRIPT="$scrDir/themeswitch.sh"
 #SDDM_SCRIPT="$HOME/bin/setsddmwallpaper"
-SDDM_SCRIPT="$HOME/.local/share/bin/setsddmwallpaper.sh"
+SDDM_SCRIPT="$scrDir/setsddmwallpaper.sh"
 LOG_FILE="$HOME/.cache/cyclewallpaperandtheme.log"
 WALL_CYCLE_FILE="$HOME/.local/share/cyclewallpapercount"
 THEME_CYCLE_FILE="$HOME/.local/share/cyclethemecount"
 THEME_CHANGE_FLAG_FILE="$HOME/.local/share/cyclethemeflag"
-THEMES_DIR="$HOME/.config/hyde/themes"
-HYDE_CONFIG="$HOME/.config/hyde/hyde.conf"
+THEMES_DIR="$hydeConfDir/themes"
+HYDE_CONFIG="$hydeConfDir/hyde.conf"
 
 # Log messages to file
 log() {
