d0eb3ea8b07eac4150cd7dbcb367b954928041e7
Anshul-007 <anshuljyoti.ict@gmail.com>
2025-01-03 22:49:08 +0530

    fix: updated using suggested change

--- Configs/.local/lib/hyde/systemupdate.sh
+++ Configs/.local/lib/hyde/systemupdate.sh
@@ -11,18 +11,30 @@ source "$scrDir/globalcontrol.sh"
 get_aurhlpr
 export -f pkg_installed
 fpk_exup="pkg_installed flatpak && flatpak update"
+# Create the folder to store the JSON file
+[ ! -f "$HYDE_RUNTIME_DIR/update_info.json" ] && mkdir -p "$HYDE_RUNTIME_DIR"
+json_file="$HYDE_RUNTIME_DIR/update_info.json"
 
 # Trigger upgrade
 if [ "$1" == "up" ] ; then
-    # trap 'pkill -RTMIN+20 waybar' EXIT
-    command="
-    fastfetch
-    $0 upgrade &
-    ${aurhlpr} -Syu
-    $fpk_exup
-    read -n 1 -p 'Press any key to continue...'
-    "
-    kitty --title systemupdate sh -c "${command}"
+    if [ -f "$json_file" ]; then
+        # refreshes the module so after you update it will reset to zero
+        trap 'pkill -RTMIN+20 waybar' EXIT
+        # Read and parse JSON in one step
+        eval "$(jq -r '@sh "official_updates=\(.official_updates) aur_updates=\(.aur_updates) flatpak_updates=\(.flatpak_updates)"' "$json_file")"  
+        command="
+        fastfetch
+        printf '[%s] %5s\n' 'Official' '$official_updates'
+        printf '[%s] %10s\n' 'AUR' '$aur_updates'
+        printf '[%s] %6s\n' 'Flatpak' '$flatpak_updates'
+        ${aurhlpr} -Syu
+        $fpk_exup
+        read -n 1 -p 'Press any key to continue...'
+        "
+        kitty --title systemupdate sh -c "${command}"
+    else
+        echo "No upgrade info found. Please run the script without parameters first."
+    fi
     exit 0
 fi
 
@@ -41,8 +53,19 @@ fi
 
 # Calculate total available updates
 upd=$(( ofc + aur + fpk ))
-[ "${1}" == upgrade ] && printf "\n[Official] %-10s\n[AUR]      %-10s\n[Flatpak]  %-10s\nEnter your password:" "$ofc" "$aur" "$fpk" && exit
+# Prepare the upgrade info as JSON format
+upgrade_info=$(cat <<EOF
+{
+    "official_updates": "$ofc",
+    "aur_updates": "$aur",
+    "flatpak_updates": "$fpk",
+    "total_updates": "$upd"
+}
+EOF
+)
 
+# Save the upgrade info as JSON file
+echo "$upgrade_info" > "$json_file"
 # Show tooltip
 if [ $upd -eq 0 ] ; then
     upd="" #Remove Icon completely
