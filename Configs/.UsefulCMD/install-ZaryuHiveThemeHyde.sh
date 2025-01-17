e14b6d78377a49ac148d18e8d5ea3506203b05d6
Niumination <123625275+Niumination@users.noreply.github.com>
2025-01-17 11:04:40 +0700

    Update install-ZaryuHiveThemeHyde.sh

--- Configs/.UsefulCMD/install-ZaryuHiveThemeHyde.sh
+++ Configs/.UsefulCMD/install-ZaryuHiveThemeHyde.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-yay -S hyde-cli-git
+yay -S hyde-cli-git --needed
 Hyde-install -d ~/HyDE -g https://github.com/Niumination/Zaryu-Hyde.git --link
-Hyde theme import ZaryuHive https://github.com/Niumination/ZaryuHive.git
+Hyde theme import ZaryuHive https://github.com/Niumination/ZaryuHive
