b942aafec95ec287f4af6e22b849a0b183ff81e5
Khing <53417443+kRHYME7@users.noreply.github.com>
2025-01-20 23:39:00 +0800

    Update swwwallselect.sh

--- Configs/.local/lib/hyde/swwwallselect.sh
+++ Configs/.local/lib/hyde/swwwallselect.sh
@@ -1,29 +1,34 @@
 #!/usr/bin/env bash
 
 #// set variables
+
 scrDir="$(dirname "$(realpath "$0")")"
 # shellcheck disable=SC1091
 source "${scrDir}/globalcontrol.sh"
 
 #// set rofi scaling
+
 rofiScale="${ROFI_WALLPAPER_SCALE}"
 [[ "${rofiScale}" =~ ^[0-9]+$ ]] || rofiScale=${ROFI_SCALE:-10}
+r_scale="configuration {font: \"JetBrainsMono Nerd Font ${rofiScale}\";}"
 # shellcheck disable=SC2154
 elem_border=$((hypr_border * 3))
 
 #// scale for monitor
+
 mon_x_res=$(hyprctl -j monitors | jq '.[] | select(.focused == true) | (.width / .scale)')
 
 #// generate config
+
 elm_width=$(((28 + 8 + 5) * rofiScale))
 max_avail=$((mon_x_res - (4 * rofiScale)))
 col_count=$((max_avail / elm_width))
-
-r_override="window { width: 100%; }
-listview { columns: ${col_count}; spacing: 5em; }
-element { border-radius: ${elem_border}px; orientation: vertical; }
-element-icon { size: 28em; border-radius: 0em; }
-element-text { padding: 1em; }"
+r_override="window{width:100%;}
+    listview{columns:${col_count};spacing:5em;}
+    element{border-radius:${elem_border}px;
+    orientation:vertical;} 
+    element-icon{size:28em;border-radius:0em;}
+    element-text{padding:1em;}"
 
 #// launch rofi menu
 
@@ -35,19 +40,20 @@ get_hashmap "${wallPathArray[@]}"
 wallListBase=()
 # shellcheck disable=SC2154
 for wall in "${wallList[@]}"; do
-    wallListBase+=("${wall##*/}")
+    wallListBase+=("${wall##*/}") # get the basename // bash way
 done
 
 # shellcheck disable=SC2154
 rofiSel=$(paste <(printf "%s\n" "${wallListBase[@]}") <(printf "|%s\n" "${wallHash[@]}") |
     awk -F '|' -v thmbDir="${thmbDir}" '{split($1, arr, "/"); print arr[length(arr)] "\x00icon\x1f" thmbDir "/" $2 ".sqre"}' |
     rofi -dmenu \
-        -theme "selector" \
+        -theme-str "${r_scale}" \
         -theme-str "${r_override}" \
-        -select "${currentWall}"
-)
+        -config "${ROFI_WALLPAPER_STYLE:-selector}" \
+        -select "${currentWall}" | xargs)
 
 #// apply wallpaper
+
 if [ -n "${rofiSel}" ]; then
     for i in "${!wallPathArray[@]}"; do
         setWall=$(find "${wallPathArray[i]}" -type f -name "${rofiSel}")
@@ -59,4 +65,5 @@ if [ -n "${rofiSel}" ]; then
     else
         notify-send -a "HyDE Alert" "Wallpaper not found"
     fi
+
 fi
