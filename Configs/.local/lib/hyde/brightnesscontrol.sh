dc81728a4acbb345a15a3166e29ab65b4deeefe4
Meshwa428 <135232056+Meshwa428@users.noreply.github.com>
2025-01-24 00:28:37 +0530

    Used weber fetcher's law for better human perception brightness

--- Configs/.local/lib/hyde/brightnesscontrol.sh
+++ Configs/.local/lib/hyde/brightnesscontrol.sh
@@ -1,76 +1,88 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-scrDir=$(dirname "$(realpath "$0")")
-# shellcheck disable=SC1091
-source "$scrDir/globalcontrol.sh"
+# Check if the script is already running
+pgrep -cf "${0##*/}" | grep -qv 1 && echo "An instance of the script is already running..." && exit 1
+
+scrDir=`dirname "$(realpath "$0")"`
+source $scrDir/globalcontrol.sh
 
 # Check if SwayOSD is installed
 use_swayosd=false
-isNotify=${BRIGHTNESS_NOTIFY:-true}
 if command -v swayosd-client >/dev/null 2>&1 && pgrep -x swayosd-server >/dev/null; then
     use_swayosd=true
 fi
 
-print_error() {
-    local cmd
-    cmd=$(basename "$0")
-    cat <<EOF
-    "${cmd}" <action> [step]
+print_error()
+{
+cat << EOF
+    $(basename ${0}) <action> [step] 
     ...valid actions are...
-        i -- <i>ncrease brightness [+5%]
-        d -- <d>ecrease brightness [-5%]
+        i -- <i>ncrease brightness [logarithmic step]
+        d -- <d>ecrease brightness [logarithmic step]
 
     Example:
-        "${cmd}" i 10    # Increase brightness by 10%
-        "${cmd}" d       # Decrease brightness by default step (5%)
+        $(basename ${0}) i 10    # Increase brightness by 10% (logarithmic scaling)
+        $(basename ${0}) d       # Decrease brightness by default logarithmic step
 EOF
 }
 
 send_notification() {
-    brightness=$(brightnessctl info | grep -oP "(?<=\()\d+(?=%)" | cat)
+    brightness=`brightnessctl info | grep -oP "(?<=\()\d+(?=%)" | cat`
     brightinfo=$(brightnessctl info | awk -F "'" '/Device/ {print $2}')
-    angle="$((((brightness + 2) / 5) * 5))"
-    # shellcheck disable=SC2154
-    ico="${iconsDir}/Wallbash-Icon/media/knob-${angle}.svg"
-    bar=$(seq -s "." $((brightness / 15)) | sed 's/[0-9]//g')
-    [[ "${isNotify}" == true ]] && notify-send -a "HyDE Notify" -r 7 -t 800 -i "${ico}" "${brightness}${bar}" "${brightinfo}"
+    angle="$(((($brightness + 2) / 5) * 5))"
+    ico="$HOME/.config/dunst/icons/vol/vol-${angle}.svg"
+    bar=$(seq -s "." $(($brightness / 15)) | sed 's/[0-9]//g')
+    notify-send -a "t2" -r 91190 -t 800 -i "${ico}" "${brightness}${bar}" "${brightinfo}"
 }
 
 get_brightness() {
     brightnessctl -m | grep -o '[0-9]\+%' | head -c-2
 }
 
-step=${BRIGHTNESS_STEPS:-5}
-step="${2:-$step}"
+adjust_brightness_logarithmically() {
+    local current_brightness=$(get_brightness)
+    local action=$1
+    local step=${2:-10} # Default step value, can be overridden
+
+    # Calculate the Weber-Fechner adjustment factor
+    local adjustment
+    if [[ "$action" == "increase" ]]; then
+        adjustment=$(awk "BEGIN {print int($current_brightness * ($step / 100.0))}")
+        new_brightness=$((current_brightness + adjustment))
+    elif [[ "$action" == "decrease" ]]; then
+        adjustment=$(awk "BEGIN {print int($current_brightness * ($step / 100.0))}")
+        new_brightness=$((current_brightness - adjustment))
+    fi
+
+    # Ensure the new brightness stays within the range of 1% to 100%
+    new_brightness=$((new_brightness < 1 ? 1 : (new_brightness > 100 ? 100 : new_brightness)))
+
+    # If brightness is below 10%, we prevent too steep reductions
+    if [ "$action" == "decrease" ] && [ "$new_brightness" -lt 10 ] && [ "$current_brightness" -gt 10 ]; then
+        new_brightness=$((current_brightness - 2))
+    fi
+
+    brightnessctl set "${new_brightness}%"
+    send_notification
+}
+
+
 
 case $1 in
-i | -i) # increase the backlight
-    if [[ $(get_brightness) -lt 10 ]]; then
-        # increase the backlight by 1% if less than 10%
-        step=1
-    fi
-
-    $use_swayosd && swayosd-client --brightness raise "$step" && exit 0
-    brightnessctl set +"${step}"%
-    send_notification
-    ;;
-d | -d) # decrease the backlight
-
-    if [[ $(get_brightness) -le 10 ]]; then
-        # decrease the backlight by 1% if less than 10%
-        step=1
-    fi
-
-    if [[ $(get_brightness) -le 1 ]]; then
-        brightnessctl set "${step}"%
-        $use_swayosd && exit 0
+i|-i)  # increase the backlight
+    if [[ $(get_brightness) -lt 10 ]] ; then
+        adjust_brightness_logarithmically "increase" 1
     else
-        $use_swayosd && swayosd-client --brightness lower "$step" && exit 0
-        brightnessctl set "${step}"%-
+        adjust_brightness_logarithmically "increase" "${2:-10}"
     fi
-
-    send_notification
     ;;
-*) # print error
+d|-d)  # decrease the backlight
+    if [[ $(get_brightness) -le 10 ]] ; then
+        adjust_brightness_logarithmically "decrease" 1
+    else
+        adjust_brightness_logarithmically "decrease" "${2:-10}"
+    fi
+    ;;
+*)  # print error
     print_error ;;
 esac
