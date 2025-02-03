4f41cf9e121cd48d362b1694f39247aa07fa90f0
Niumination <123625275+Niumination@users.noreply.github.com>
2025-02-03 16:06:19 +0700

    update

--- Configs/.UsefulCMD/auto-rotate-hyprland.sh
+++ Configs/.UsefulCMD/auto-rotate-hyprland.sh
@@ -5,6 +5,6 @@ cd iio-sensor-proxy
 meson _build -Dprefix=/usr
 ninja -v -C _build install
 
-yay -S iio-hyprland
+yay -S iio-hyprland-git
 echo exec-once = iio-hyprland | tee -a ~/.config/hypr/userprefs.conf
 echo monitor=eDP-1,preferred,auto,1,transform,0 | tee -a ~/.config/hypr/userprefs.conf
