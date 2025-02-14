36405dfe04a7032830f4f7f4296739bf487ac3c6
Niumination <niumination@gmail.com>
2025-02-14 22:58:30 +0700

    create osk.sh

--- Configs/.UsefulCMD/nix-determinate.sh
+++ Configs/.UsefulCMD/nix-determinate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix |
-  sh -s -- install
+  sh -s -- install --determinate
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
@@ -8,6 +8,6 @@ nix-shell '<home-manager>' -A install
 mkdir -p ~/GitHub
 cd ~/GitHub
 git clone --depth 1 https://github.com/niumination/hyprnix.git
-cd hyprnix
-nix flake update
-home-manager switch --flake .#hyprzaryu
+#cd hyprnix
+#nix flake update
+#home-manager switch --flake .#hyprzaryu
