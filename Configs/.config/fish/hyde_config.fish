0669a03e343420721237b5f2591f4659373fe526
Metalhearf <6446231+Metalhearf@users.noreply.github.com>
2025-04-04 11:40:46 +0200

    refactor: fish: streamline XDG variable setup and use xdg-user-dir for user directories

--- Configs/.config/fish/hyde_config.fish
+++ Configs/.config/fish/hyde_config.fish
@@ -12,7 +12,7 @@
 # HyDE's fish env configuration
 # This file is sourced by fish on startup
 
-# ensure that the XDG variables are set
+# Ensure XDG base directories are set, using defaults if not already defined
 if test -z "$XDG_CONFIG_HOME"
     set -gx XDG_CONFIG_HOME "$HOME/.config"
 end
@@ -37,42 +37,26 @@ if test -z "$XDG_CACHE_HOME"
     set -gx XDG_CACHE_HOME "$HOME/.cache"
 end
 
-if test -z "$XDG_DESKTOP_DIR"
-    set -gx XDG_DESKTOP_DIR "$HOME/Desktop"
+# Load user-defined directories from user-dirs.dirs if available
+if test -f "$XDG_CONFIG_HOME/user-dirs.dirs"
+    source "$XDG_CONFIG_HOME/user-dirs.dirs"
 end
 
-if test -z "$XDG_DOWNLOAD_DIR"
-    set -gx XDG_DOWNLOAD_DIR "$HOME/Downloads"
-end
+# Dynamically retrieve user directories using xdg-user-dir
+set -gx XDG_DESKTOP_DIR (xdg-user-dir DESKTOP)
+set -gx XDG_DOWNLOAD_DIR (xdg-user-dir DOWNLOAD)
+set -gx XDG_TEMPLATES_DIR (xdg-user-dir TEMPLATES)
+set -gx XDG_PUBLICSHARE_DIR (xdg-user-dir PUBLICSHARE)
+set -gx XDG_DOCUMENTS_DIR (xdg-user-dir DOCUMENTS)
+set -gx XDG_MUSIC_DIR (xdg-user-dir MUSIC)
+set -gx XDG_PICTURES_DIR (xdg-user-dir PICTURES)
+set -gx XDG_VIDEOS_DIR (xdg-user-dir VIDEOS)
 
-if test -z "$XDG_TEMPLATES_DIR"
-    set -gx XDG_TEMPLATES_DIR "$HOME/Templates"
-end
-
-if test -z "$XDG_PUBLICSHARE_DIR"
-    set -gx XDG_PUBLICSHARE_DIR "$HOME/Public"
-end
-
-if test -z "$XDG_DOCUMENTS_DIR"
-    set -gx XDG_DOCUMENTS_DIR "$HOME/Documents"
-end
-
-if test -z "$XDG_MUSIC_DIR"
-    set -gx XDG_MUSIC_DIR "$HOME/Music"
-end
-
-if test -z "$XDG_PICTURES_DIR"
-    set -gx XDG_PICTURES_DIR "$HOME/Pictures"
-end
-
-if test -z "$XDG_VIDEOS_DIR"
-    set -gx XDG_VIDEOS_DIR "$HOME/Videos"
-end
-
+# Other settings
 if test -z "$LESSHISTFILE"
     set -gx LESSHISTFILE "/tmp/less-hist"
 end
 
-if test -z "$PARALLEL_HOME"
-    set -gx PARALLEL_HOME "$XDG_CONFIG_HOME/parallel"
-end
+set -gx PARALLEL_HOME "$XDG_CONFIG_HOME/parallel"
+set -gx WGETRC "$XDG_CONFIG_HOME/wgetrc"
+set -gx SCREENRC "$XDG_CONFIG_HOME/screen/screenrc"
