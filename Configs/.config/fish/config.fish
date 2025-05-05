b6fc5dfce51c7c8176dc4fe8fdcc24cfacd1916d
prime-run <prime0run@gmail.com>
2025-05-05 14:56:30 +0000

    add ffcd ffe and ffec to main conf.fish

--- Configs/.config/fish/config.fish
+++ Configs/.config/fish/config.fish
@@ -13,6 +13,116 @@ end
 if type -q fzf
     fzf --fish | source 
 end
+
+function ffcd
+    set initial_query
+    set max_depth 7
+    if set -q argv[1]
+        set initial_query $argv[1]
+    end
+
+    set fzf_options '--preview=ls -p {} | grep /' \
+                    '--preview-window=right:60%' \
+                    '--height' '80%' \
+                    '--layout=reverse' \
+                    '--preview-window' 'right:60%' \
+                    '--cycle'
+
+    if set -q initial_query
+        set fzf_options $fzf_options "--query=$initial_query"
+    end
+
+
+    set selected_dir (find . -maxdepth $max_depth \( -name .git -o -name node_modules -o -name .venv -o -name target -o -name .cache \) -prune -o -type d -print 2>/dev/null | fzf $fzf_options)
+
+    if test -n "$selected_dir"; and test -d "$selected_dir"
+        cd "$selected_dir"; or return 1
+    else
+        return 1
+    end
+end
+
+
+
+function ffe
+    set initial_query
+    if set -q argv[1]
+        set initial_query $argv[1]
+    end
+
+    set fzf_options '--height' '80%' \
+                    '--layout' 'reverse' \
+                    '--preview-window' 'right:60%' \
+                    '--cycle'
+
+    if set -q initial_query
+        set fzf_options $fzf_options "--query=$initial_query"
+    end
+
+    set max_depth 5
+
+    set selected_file (find . -maxdepth $max_depth -type f 2>/dev/null | fzf $fzf_options)
+
+    if test -n "$selected_file"; and test -f "$selected_file"
+        nvim "$selected_file"
+    else
+        return 1
+    end
+end
+
+
+function ffe
+    set initial_query
+    if set -q argv[1]
+        set initial_query $argv[1]
+    end
+
+    set fzf_options '--height' '80%' \
+                    '--layout' 'reverse' \
+                    '--preview-window' 'right:60%' \
+                    '--cycle'
+
+    if set -q initial_query
+        set fzf_options $fzf_options "--query=$initial_query"
+    end
+
+    set max_depth 5
+
+    set selected_file (find . -maxdepth $max_depth -type f 2>/dev/null | fzf $fzf_options)
+
+    if test -n "$selected_file"; and test -f "$selected_file"
+        nvim "$selected_file"
+    else
+        return 1
+    end
+end
+
+function ffec
+    set grep_pattern ""
+    if set -q argv[1]
+        set grep_pattern $argv[1]
+    end
+
+    set fzf_options '--height' '80%' \
+                    '--layout' 'reverse' \
+                    '--preview-window' 'right:60%' \
+                    '--cycle' \
+                    '--preview' 'bat --color always {}' \
+                    '--preview-window' 'right:60%'
+
+    set selected_file (grep -irl -- "$grep_pattern" ./ 2>/dev/null | fzf $fzf_options)
+
+    if test -n "$selected_file"
+        nvim "$selected_file"
+    else
+        echo "No file selected or search returned no results."
+    end
+end
+
+
+
+
+
 # example integration with bat : <cltr+f>
 # bind -M insert \ce '$EDITOR $(fzf --preview="bat --color=always --plain {}")' 
 
