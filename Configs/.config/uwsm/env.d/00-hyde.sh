0b5d2bef13c63cc6b334b9907a9cfe187bfc4094
Olaipai <9062059+Olaipai@users.noreply.github.com>
2025-08-14 16:13:31 +0200

    Changes to UWSM config. Moved vars to correct dirs adhering to documentation (HYPR* in env-compositor) and theming etc into env. Deleted XDG default dirs, these are already managed by UWSM and not compositor.

--- Configs/.config/uwsm/env.d/00-hyde.sh
+++ Configs/.config/uwsm/env.d/00-hyde.sh
@@ -3,13 +3,6 @@
 # Basic PATH prepending (user local bin)
 PATH="$HOME/.local/bin:$PATH"
 
-# XDG Base Directory Specification variables with defaults
-XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
-XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
-XDG_DATA_DIRS="${XDG_DATA_DIRS:-$XDG_DATA_HOME:/usr/local/share:/usr/share}"
-XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
-XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
-
 # XDG User Directories (fallback to xdg-user-dir command if available)
 if command -v xdg-user-dir >/dev/null 2>&1; then
   XDG_DESKTOP_DIR="${XDG_DESKTOP_DIR:-$(xdg-user-dir DESKTOP)}"
@@ -29,13 +22,30 @@ LESSHISTFILE="${LESSHISTFILE:-/tmp/less-hist}"
 PARALLEL_HOME="$XDG_CONFIG_HOME/parallel"
 SCREENRC="$XDG_CONFIG_HOME/screen/screenrc"
 
-# HyDEs Compositor Configuration
-export HYPRLAND_CONFIG="${XDG_DATA_HOME:-$HOME/.local/share}/hypr/hyprland.conf"
-
 
 # Export all variables
 export PATH \
-  XDG_CONFIG_HOME XDG_DATA_HOME XDG_DATA_DIRS XDG_STATE_HOME XDG_CACHE_HOME \
   XDG_DESKTOP_DIR XDG_DOWNLOAD_DIR XDG_TEMPLATES_DIR XDG_PUBLICSHARE_DIR \
   XDG_DOCUMENTS_DIR XDG_MUSIC_DIR XDG_PICTURES_DIR XDG_VIDEOS_DIR \
   LESSHISTFILE PARALLEL_HOME SCREENRC
+
+# Toolkit Backend Variables - https://wiki.hyprland.org/Configuring/Environment-variables/#toolkit-backend-variables
+# GDK_BACKEND="${GDK_BACKEND:-wayland,x11,*}"       # GTK: Use wayland if available. If not: try x11, then any other GDK backend.
+# SDL_VIDEODRIVER="${SDL_VIDEODRIVER:-wayland}" # Run SDL2 applications on Wayland. Remove or set to x11 if games that provide older versions of SDL cause compatibility issues
+# CLUTTER_BACKEND="${CLUTTER_BACKEND:-wayland}" # Clutter package already has wayland enabled, this variable will force Clutter applications to try and use the Wayland backend
+
+# # Qt Variables  - https://wiki.hyprland.org/Configuring/Environment-variables/#qt-variables
+
+QT_QPA_PLATFORM="${QT_QPA_PLATFORM:-wayland;xcb}" # Qt: Use wayland if available, fall back to x11 if not.
+QT_AUTO_SCREEN_SCALE_FACTOR="${QT_AUTO_SCREEN_SCALE_FACTOR:-1}"                 # Enables automatic scaling, based on the monitor’s pixel density
+QT_WAYLAND_DISABLE_WINDOWDECORATION="${QT_WAYLAND_DISABLE_WINDOWDECORATION:-1}" # Disables window decorations on Qt applications
+QT_QPA_PLATFORMTHEME="${QT_QPA_PLATFORMTHEME:-qt6ct}"                           # Tells Qt based applications to pick your theme from qt5ct, use with Kvantum.
+
+# # HyDE Environment Variables -
+
+MOZ_ENABLE_WAYLAND="${MOZ_ENABLE_WAYLAND:-1}"                        # Enable Wayland for Firefox
+GDK_SCALE="${GDK_SCALE:-1}"                                          # Set GDK scale to 1, for Xwayland on HiDPI displays
+ELECTRON_OZONE_PLATFORM_HINT="${ELECTRON_OZONE_PLATFORM_HINT:-auto}" # Set Electron Ozone Platform Hint to auto, for Electron apps on Wayland
+
+export ELECTRON_OZONE_PLATFORM_HINT GDK_SCALE MOZ_ENABLE_WAYLAND QT_QPA_PLATFORMTHEME \
+    QT_WAYLAND_DISABLE_WINDOWDECORATION QT_QPA_PLATFORM QT_AUTO_SCREEN_SCALE_FACTOR
