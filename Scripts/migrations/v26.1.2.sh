291c479bef5e6457ff1074719d85300e390adec2
Ilya Bogdanov <zeerayne1337@gmail.com>
2026-01-13 11:07:19 +0300

    fix: add migration improvements proposed by @coderabbitai

--- Scripts/migrations/v26.1.2.sh
+++ Scripts/migrations/v26.1.2.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env sh
+
 echo "'hyq' binary is moved to new location"
 echo "Removing old binary from '~/.local/lib/hyde/hyq'"
-rm $HOME/.local/lib/hyde/hyq
+rm -f$HOME/.local/lib/hyde/hyq
